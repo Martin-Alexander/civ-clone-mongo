5a06a9b9d57ad1d9fca263d71e54bc704550c59d
Martin-Alexander <martingianna@gmail.com>
2018-04-22 17:14:41 -0400

    ts conversion of BoardMethods

---
+++ app/javascript/game/services/a_star/board_methods.ts
@@ -0,0 +1,45 @@
+import Square from "../../models/square";
+import AStarSquareCollection from "./a_star_square_collection";
+import AStarSquare from "./a_star_square";
+
+export default class BoardMethods {
+  public squares: AStarSquareCollection;
+
+  public neighbours(square: Square, radius?: number): Square[] {
+    if (typeof radius == 'undefined') {
+      var radius = 1;
+    }
+
+    const xRange = Array.from(new Array(radius * 2 + 1), (x, i) => i + -radius);
+    const yRange = Array.from(new Array(radius * 2 + 1), (x, i) => i + -radius);
+    const neighbourSquares = [];
+  
+    xRange.forEach((x) => {
+      yRange.forEach((y) => {
+        if (!(                                 // None of the following are true:
+          (x === 0 && y === 0)              || // is original square
+          x + square.x < 0                  || // is...
+          y + square.y < 0                  || // outside...
+          x + square.x > this.squares.size  || // board...
+          y + square.y > this.squares.size     // bounderies
+        )) {
+          neighbourSquares.push(this.findSquare(x + square.x, y + square.y));
+        }
+      });
+    });
+
+    return neighbourSquares;
+  };
+
+  public neighboursAndCurrentSquare(square: Square, radius?: number): Square[] {
+    const neighbourSquares = this.neighbours(square, radius);
+    neighbourSquares.push(square);
+  
+    return neighbourSquares;
+  };
+
+  public findSquare(x: number, y: number): AStarSquare {
+    return this.squares[y * (this.squares.size + 1) + x];
+  };
+}  
+
--- app/javascript/game/services/a_star/calculations/a_star.js
+++ app/javascript/game/services/a_star/calculations/a_star.js
@@ -1,5 +1,5 @@
 import AStarSquareCollection     from "./../a_star_square_collection";
-import { BoardMethods }          from "./../board_methods";
+import BoardMethods              from "./../board_methods";
 import AStarSquare               from "./../a_star_square";
 
 // This is a basic implementation of the A* algorithm that is designed to find a path between two
