7a0319346dc45df516ed2bb400253d2199dcb0a9
Martin-Alexander <martingianna@gmail.com>
2018-05-06 01:46:46 -0400

    basic structure of zone of control system

---
+++ app/models/square/modules/zone_of_control.rb
@@ -0,0 +1,17 @@
+module Square
+  module Modules
+    module ZoneOfControl
+      
+      # Get the control of an area
+      def control
+
+      end
+
+      class Control
+        def initialize(square)
+          
+        end
+      end
+    end
+  end
+end
