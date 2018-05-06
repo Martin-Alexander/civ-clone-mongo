3a8725a4ad3baf4b7d19a905d7f1ebe4c5454e4f
Martin-Alexander <martingianna@gmail.com>
2018-05-05 23:09:59 -0400

    added some weird color change for units who have expended all their moves

--- app/javascript/game/models/unit.ts
+++ app/javascript/game/models/unit.ts
@@ -28,6 +28,10 @@ export default class Unit {
 
   public render(context: CanvasRenderingContext2D, UI: UserInterface): void {
     context.beginPath();
+    context.ellipse(0, UI.tileHeight / 2, UI.tileWidth / 4, UI.tileHeight / 4,
+      0, Math.PI * 0, Math.PI * 2);
+    context.fillStyle = "grey";
+    context.fill();
     context.ellipse(0, UI.tileHeight / 2, UI.tileWidth / 4, UI.tileHeight / 4, 0, Math.PI * 1.25, Math.PI * 0.25);
     context.fillStyle = this.playerColor();
     context.fill();
@@ -38,28 +42,41 @@ export default class Unit {
     this.drawStrength(context, UI)
   };
 
-  private playerColor(): string {   
+  private playerColor(): string {
+    const opacity = this.opacity();
+
     const unitPlayerColorLookup: { [key: number]: string } = {
-      1: "red",
-      2: "blue",
-      3: "pink",
-      4: "purple",
-      5: "orange"
+      1: `rgba(255, 0, 0, ${opacity}`,
+      2: `rgba(0, 0, 255, ${opacity}`,
+      3: `rgb(255, 192, 203, ${opacity})`,
+      4: `rgb(128, 0, 128, ${opacity})`,
+      5: `rgb(255, 165, 0, ${opacity})`
     };
 
     return unitPlayerColorLookup[this.player_number]
   };
 
   private typeColor(): string {
+    // const opacity = this.opacity();
+    const opacity = 1;
+
     const unitTypeColorLookup: { [key: string]: string } = {
-      worker: "#47220c",
-      infantry: "#020366",
-      tank: "#053d05",
-      ship: "#5b5b5b"
+      worker: `rgb(71,34,12, ${opacity})`,
+      infantry: `rgb(2,3,102, ${opacity})`,
+      tank: `rgb(5,61,5, ${opacity})`,
+      ship: `rgb(91,91,91, ${opacity})`
     };
 
     return unitTypeColorLookup[this.type];
   };
+
+  private opacity(): string {
+    if (this.moves === 0) {
+      return "0.4";
+    } else {
+      return "1";
+    }
+  }
   
   drawStrength(context: CanvasRenderingContext2D, UI: UserInterface): void | boolean {
     if (this.type === UnitType.worker) return false;
