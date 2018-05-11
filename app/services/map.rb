8d477f8d4b8ae2a6dc6845789a8a220d182292fb
Martin-Alexander <martingianna@gmail.com>
2018-05-11 15:38:19 -0400

    added services folder and Map class to handle map generation

---
+++ app/services/map.rb
@@ -0,0 +1,9 @@
+class Map
+  module ClassMethods
+    def generate(options = {})
+      puts "TODO"
+    end
+  end
+
+  extend ClassMethods
+end
--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module CivCloneMongo
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.autoload_paths += ["#{config.root}/lib/rules"]
-    config.eager_load_paths += ["#{config.root}/lib/rules"]
+    config.autoload_paths += ["#{config.root}/lib/rules", "#{config.root}/app/services"]
+    config.eager_load_paths += ["#{config.root}/lib/rules", "#{config.root}/app/services"]
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
