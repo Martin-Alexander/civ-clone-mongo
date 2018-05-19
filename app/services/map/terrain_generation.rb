eb1c783d1f60962a818e92525c968dce8f2813a2
Martin-Alexander <martingianna@gmail.com>
2018-05-19 18:32:31 -0400

    added  option to terrain generation

--- app/services/map/terrain_generation.rb
+++ app/services/map/terrain_generation.rb
@@ -5,6 +5,8 @@ module Map
     def generate(terrain, options)
       # squares.generate "water", coverage: { percent: 50 }, distinct: true
 
+      @passes = options[:passes] || 1
+
       coverage = if options[:coverage].is_a?(Hash)
         ((options[:coverage][:percent] / 100.0) * self.count).floor
       else
@@ -14,14 +16,16 @@ module Map
       terrain_generation_condition = TerrainGenerationCondition.new
       yield terrain_generation_condition
 
-      if options[:distinct] || options[:distinct].nil?
-        self.sample(coverage).each do |square|
-          square.terrain = terrain if terrain_generation_condition.pass?(square)
-        end
-      else
-        coverage.times do
-          square = self.sample
-          square.terrain = terrain if terrain_generation_condition.pass?(square)
+      @passes.times do
+        if options[:distinct] || options[:distinct].nil?
+          self.sample(coverage).each do |square|
+            square.terrain = terrain if terrain_generation_condition.pass?(square)
+          end
+        else
+          coverage.times do
+            square = self.sample
+            square.terrain = terrain if terrain_generation_condition.pass?(square)
+          end
         end
       end
     end
