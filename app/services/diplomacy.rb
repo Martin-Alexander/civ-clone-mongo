c709c920edf637f8295e8f5d5af8e1533dd438b2
Martin-Alexander <martingianna@gmail.com>
2018-06-14 02:12:29 -0400

    begun work on diplomacy service

---
+++ app/services/diplomacy.rb
@@ -0,0 +1,17 @@
+# class Diplomacy
+#   module ClassMethods
+#     def mutual_allies(*players)
+#       players.combination(2).all? do |player_one, player_to|
+
+#       end
+#     end
+
+#     def mutual_enemies(*players)
+#     end
+
+#     def mutual_neutral(*players)
+#     end        
+#   end
+
+#   extend ClassMethods
+# end
