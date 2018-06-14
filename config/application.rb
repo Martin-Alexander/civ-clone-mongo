fa9bda7aa8a7e532654ec74a245d65bb2bbab11f
Martin-Alexander <martingianna@gmail.com>
2018-06-14 01:59:52 -0400

    initializes are loaded too late, apparently

--- config/application.rb
+++ config/application.rb
@@ -29,3 +29,10 @@ module CivCloneMongo
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+model_directories = Dir.glob(Rails.root.join("app/models/*")).select { |f| File.directory? f }
+
+model_directories.each do |directory|
+  directory_name = directory.split("/").last
+  ActiveSupport.autoload(directory_name.capitalize, directory + "/" + directory_name)
+end
