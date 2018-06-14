68c4806d7fadb830054d78a74e56789342f50ec0
Martin-Alexander <martingianna@gmail.com>
2018-06-13 23:47:44 -0400

    colourful logging of delete and insert mongo queries

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -7,9 +7,13 @@ class MongoidColourfulLogger < Logger
       begin
         if message.split(" | ")[3] == "STARTED"
           if message.split(" | ")[4].include?("{\"find")
-            new_message = "#{message.split(' | ')[0...-1].join(' | ')} | \e[1;94m#{query_portion}\e[0;97m"
+            new_message = "\e[1;94m#{message.split(' | ')[0...-1].join(' | ')} | #{query_portion}\e[0;97m"
           elsif message.split(" | ")[4].include?("{\"update")
-            new_message = "#{message.split(' | ')[0...-1].join(' | ')} | \e[1;93m#{query_portion}\e[0;97m"
+            new_message = "\e[1;93m#{message.split(' | ')[0...-1].join(' | ')} | #{query_portion}\e[0;97m"
+          elsif message.split(" | ")[4].include?("{\"insert")
+            new_message = "\e[1;92m#{message.split(' | ')[0...-1].join(' | ')} | #{query_portion}\e[0;97m"
+          elsif message.split(" | ")[4].include?("{\"delete")
+            new_message = "\e[1;91m#{message.split(' | ')[0...-1].join(' | ')} | #{query_portion}\e[0;97m"            
           else
             new_message = query_portion
           end
